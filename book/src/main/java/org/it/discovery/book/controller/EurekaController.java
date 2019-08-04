9d71c58cde0d0836693cf85da385c50562f5b160
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-08-04 17:18:34 +0300

    Change Eureka to Zookeeper (not working yet!)

--- book/src/main/java/org/it/discovery/book/controller/EurekaController.java
+++ book/src/main/java/org/it/discovery/book/controller/EurekaController.java
@@ -1,12 +1,6 @@
 package org.it.discovery.book.controller;
 
-import com.netflix.appinfo.InstanceInfo;
-import com.netflix.discovery.EurekaClient;
-import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,22 +9,22 @@ public class EurekaController {
 
   private static final String CONFIG_SERVER = "config-server";
 
-  private final EurekaClient eurekaClient;
-
-  @GetMapping("/api/v1/eureka/config-server")
-  public String configServer() {
-    return eurekaClient.getApplication(CONFIG_SERVER).getInstances()
-        .stream()
-        .map(Objects::toString)
-        .collect(Collectors.joining(";"));
-  }
-
-  @GetMapping("/api/v1/eureka/test")
-  public String test() {
-    List<InstanceInfo> instancesById = eurekaClient.getInstancesById(CONFIG_SERVER);
-    return instancesById
-        .stream()
-        .map(InstanceInfo::toString)
-        .collect(Collectors.joining(";"));
-  }
+//  private final EurekaClient eurekaClient;
+//
+//  @GetMapping("/api/v1/eureka/config-server")
+//  public String configServer() {
+//    return eurekaClient.getApplication(CONFIG_SERVER).getInstances()
+//        .stream()
+//        .map(Objects::toString)
+//        .collect(Collectors.joining(";"));
+//  }
+//
+//  @GetMapping("/api/v1/eureka/test")
+//  public String test() {
+//    List<InstanceInfo> instancesById = eurekaClient.getInstancesById(CONFIG_SERVER);
+//    return instancesById
+//        .stream()
+//        .map(InstanceInfo::toString)
+//        .collect(Collectors.joining(";"));
+//  }
 }
--- gateway/src/main/java/org/it/discovery/zuul/ZuulApplication.java
+++ gateway/src/main/java/org/it/discovery/zuul/ZuulApplication.java
@@ -2,10 +2,10 @@ package org.it.discovery.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class ZuulApplication {
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
